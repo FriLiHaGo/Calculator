261c4bb80cd1d166c74bf10e3dc80d35e4594f56
Погудин Миша <pogudin_m@mail.ru>
2018-01-24 21:23:34 +0300

    Forms Calculator

---
+++ Calculator/DesktopCalc/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DesktopCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbOperations = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnCalc.Enabled = false;
+            this.btnCalc.Location = new System.Drawing.Point(87, 166);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 29);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Выполнить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // tbData
+            // 
+            this.tbData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbData.Enabled = false;
+            this.tbData.Location = new System.Drawing.Point(87, 131);
+            this.tbData.Multiline = true;
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(316, 29);
+            this.tbData.TabIndex = 8;
+            this.tbData.TextChanged += new System.EventHandler(this.tbData_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Реузльтат";
+            // 
+            // lbOperations
+            // 
+            this.lbOperations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbOperations.FormattingEnabled = true;
+            this.lbOperations.Location = new System.Drawing.Point(87, 3);
+            this.lbOperations.Name = "lbOperations";
+            this.lbOperations.Size = new System.Drawing.Size(316, 122);
+            this.lbOperations.TabIndex = 11;
+            this.lbOperations.SelectedIndexChanged += new System.EventHandler(this.lbOperations_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Операции";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lbResult.Location = new System.Drawing.Point(87, 198);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(59, 36);
+            this.lbResult.TabIndex = 13;
+            this.lbResult.Text = "Реузльтат";
+            this.lbResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Данные";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.68966F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.31035F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lbOperations, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbData, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbResult, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnCalc, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(406, 234);
+            this.tableLayoutPanel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 335);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbOperations;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Calculator/DesktopCalc/Form1.cs
@@ -0,0 +1,54 @@
+﻿using CalcLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopCalc
+{
+    public partial class Form1 : Form
+    {
+        private Calc Calc { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Calc = new Calc();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbOperations.Items.Clear();
+            lbOperations.Items.AddRange(Calc.GetOperations());
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (lbOperations.SelectedItem == null)
+            {
+                return;
+            } 
+
+            var oper = lbOperations.SelectedItem.ToString();
+
+            var result = Calc.Exec(oper, tbData.Text.Trim().Split(' '));
+
+            lbResult.Text = result.ToString();
+        }
+
+        private void lbOperations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbData.Enabled = lbOperations.SelectedItem != null;
+        }
+
+        private void tbData_TextChanged(object sender, EventArgs e)
+        {
+            btnCalc.Enabled = !string.IsNullOrWhiteSpace(tbData.Text);
+        }
+    }
+}
