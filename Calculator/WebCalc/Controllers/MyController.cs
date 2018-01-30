47a69181da3a0db3c724b2746373ae113d42242e
Погудин Миша <pogudin_m@mail.ru>
2018-01-30 10:05:22 +0300

    ASP.NET v2

---
+++ Calculator/WebCalc/Controllers/MyController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCalc.Controllers
+{
+    public class MyController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(long id)
+        {
+            ViewData.Model = id.ToString();
+
+            ViewData["age"] = 21;
+            ViewData.Add("isAdmin", false);
+
+            ViewBag.lastname = "Погудин";
+            ViewBag.Cities = new string[] { "Kirov", "Piter" };
+
+            return View("Name");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string firstname)
+        {
+            ViewData["age"] = 21;
+            ViewData.Add("isAdmin", false);
+
+            ViewBag.lastname = "Погудин";
+            ViewBag.Cities = new string[] { "Kirov", "Piter" };
+
+            return View("Name", model: firstname);
+        }
+
+    }
+}
---
+++ Calculator/WebCalc/Extentions/MyHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCalc.Extentions
+{
+    public static class MyHelpers
+    {
+        public static MvcHtmlString ListerFlat(this HtmlHelper html, string[] items)
+        {
+            var ul = new TagBuilder("ul");
+            foreach (var item in items)
+            {
+                var li = new TagBuilder("li");
+                li.InnerHtml = item;
+
+                ul.InnerHtml += li;
+            }
+            return MvcHtmlString.Create(ul.ToString());
+        }
+    }
+}
